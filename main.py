5868423776647d070ca29ce6890bf69fb9da28ef
Shamallah <shamallah.chibeu@gmail.com>
2026-09-11 21:42:13 +0300

    Allow custom domain shamallah.dev in CORS settings

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://shamallah-website-frontend.vercel.app"],
+    allow_origins=["http://localhost:3000", "https://shamallah-website-frontend.vercel.app", "https://shamallah.dev", "https://www.shamallah.dev"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
